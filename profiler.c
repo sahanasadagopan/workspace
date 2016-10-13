adc49ed0e512fbef58af2ca8c128ebcdc18249da
sahanasadagopan <sahana.sadagopan@gmail.com>
2016-10-12 20:07:07 -0600

    Update profiler.c

--- profiler.c
+++ profiler.c
@@ -4,73 +4,37 @@
 #include <stdlib.h>
 #include "memory.h"
 #include "sys/time.h"
-void reverse(char *str, int len)
+
+void add_item(circbuffer *cb,uint8_t item)
 {
-    int i=0, j=len-1, temp;
-    while (i<j)
-    {
-        temp = *(str+i);
-        *(str+i) = *(str+j);
-        *(str+j) = temp;
-        i++; j--;
-    }
-}
-
-// Converts integer to ascii
-int my_itoa(char *str, int data, int d)
-{
-    int i = 0,count=0;
-    if(data<0)
-    {
-        count++;
-        data=-data;
-    }
-    while (data)
-    {
-        *(str+i++) = (data%10) + '0';
-        data = data/10;
-    }
-
-    // If number of digits required is more, then
-    // add 0s at the beginning
-    while (i < d)
-        *(str+i++) = '0';
-    if(count==1)
-        *(str+i++)='-';
-    reverse(str, i);
-    *(str+i) = '\0';
-    return i;
-}
-
-// Converts a floating point number to string.
-void my_ftoa(float data, char *str, int number_after_decimal)
-{
-    // Extract integer part
-    int before_decimal = (int)data;
-    
-    float after_decimal;
-    // Extract floating part
-    if (data<0)
-	 after_decimal= (float)before_decimal-data;
-    
-    else
-    	after_decimal = data-(float)before_decimal;
-
-    // convert integer part to string
-    int i = my_itoa(str,before_decimal, 0);
-
-    // check for numbers after point
-    if (number_after_decimal != 0)
-    {
-        *(str+i) = '.';  // add dot
-
-        // Get the value of fraction part
-        after_decimal = after_decimal * pow(10, number_after_decimal);
-
-        my_itoa(str+i+1,(int)after_decimal, number_after_decimal);
-    }
+	uint8_t buffcheck=buff_full(cb);
+	if(buffcheck)
+	{
+		char s[]="Buffer is full, wrap around is done";
+		//Log0(s,strlen(s));
+		cb->num_elements=0;
+		cb->head=cb->buffer;
+	}
+		*cb->head=item;
+		char s[]="Success";
+		//Log0(s,strlen(s));
+		//printf("success");
+		cb->head++;
+		cb->num_elements++;
 }
 void main(){
+	circbuffer cb,b;
+	uint8_t i,length,item;
+	//printf("\nEnter the length of the buffer");
+	//scanf("%c",&length);
+	char arr[30]="This is integer";
+	char arr1[30];
+	cb.size=10;
+	cb.num_elements=0;
+	cb.buffer=arr1;
+	cb.tail=cb.buffer;
+	cb.head=cb.buffer;
+	
 int t;
 	float s;
 	int dr,cr;

27f3c1df599517f16b205cbfa1b89ae56732d0be
sahanasadagopan <sahana.sadagopan@gmail.com>
2016-10-12 20:32:36 -0600

    Update profiler.c

--- profiler.c
+++ profiler.c
@@ -5,25 +5,11 @@
 #include "memory.h"
 #include "sys/time.h"
 #include "buffer.h"
-
-void del_item(circbuffer *cb)
+void release(char *s)
 {
-	/*uint8_t buffcheck=buff_empty(cb);
-	if(buffcheck)
-	{
-		char s[]="Buffer empty";
-		//Log0(s,strlen(s));
-	}
-	else
-	{*/
-		uint8_t item;
-		item=*cb->tail;
-		char s[]="\nDeleted item\n";
-		//Log0(s,strlen(s));
-		cb->tail++;
-		cb->num_elements--;
-	//}
+	free(s);
 }
+
 void main(){
 	circbuffer cb,b;
 	uint8_t i,length,item;
@@ -38,18 +24,18 @@ void main(){
 	cb.head=cb.buffer;
 	
 int t;
-	float s;
+	char s;
 	int dr,cr;
 int8_t src;
 int32_t dst;
-struct timeval add_item_start,add_item_end,del_item_start,del_item_end,my_ftoa_start,my_ftoa_end,atoi_start,atoi_end, itoa_start,itoa_end,ftoa_start,ftoa_end;
+struct timeval release_start,release_end,del_item_start,del_item_end,my_ftoa_start,my_ftoa_end,atoi_start,atoi_end, itoa_start,itoa_end,ftoa_start,ftoa_end;
 long time,mtime, secs, usecs;
 int32_t base=10;
-gettimeofday(&del_item_start, NULL); // gets time in the start
-del_item(&cb); // executes function my_memove
-gettimeofday(&del_item_end, NULL);// gets time in the end
-secs  = del_item_end.tv_sec  - del_item_start.tv_sec;
-usecs = del_item_end.tv_usec - del_item_start.tv_usec;
+gettimeofday(&release_start, NULL); // gets time in the start
+release(s); // executes function my_memove
+gettimeofday(&release_end, NULL);// gets time in the end
+secs  = release_end.tv_sec  - release_start.tv_sec;
+usecs = release_end.tv_usec - release_start.tv_usec;
 time = ((secs*1000000)+usecs); // calculates time in us
-printf("Elapsed time for del_item for : %ld usec\n",time);
+printf("Elapsed time for free for : %ld usec\n",time);
 }

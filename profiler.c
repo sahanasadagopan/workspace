27e9bc12f3d4eb00a0a326cfbfb33e05bcc70340
sahanasadagopan <sahana.sadagopan@gmail.com>
2016-10-12 18:42:34 -0600

    Add files via upload

--- profiler.c
+++ profiler.c
@@ -3,46 +3,36 @@
 #include <string.h>
 #include "memory.h"
 #include "sys/time.h"
-int8_t my_memmove(uint8_t *src,uint8_t *dst,uint32_t length)
-{	
-	if(src!=NULL && dst!=NULL)
-	{
+int8_t my_memzero(uint8_t *src,uint32_t length)
+{		
+	if(src!=NULL)
+	{	
 		while(src<src+length)
 		{
-			if(dst>src && dst<src+length)
-			{
-				dst=dst+length;
-				src=src+length;
-				*dst=*src+length;
-				dst--;
-				src--;
-			}
-			else
-			{
-				*dst=*src;
-				src++;
-				dst++;
-				length--;
-			}
+			*src=0;
+			src++;	
+			length--;							
 		}
+	*src=0;	
 	return 1;
 	}
-	else return 0;
-	
-	
+	else 
+	{
+		*src=0;		
+		return 0;
+	}
 }
-
 void main(){
 int t;
 uint8_t src[5000], dst[5000];
 struct timeval my_memmove_start,my_memmove_end,memmove_start,memmove_end,my_memzero_start,my_memzero_end,memset_start,memset_end, my_reverse_start, my_reverse_end;
 long time,mtime, secs, usecs;
-int32_t length=5000;
-gettimeofday(&my_memmove_start, NULL); // gets time in the start
-my_memmove(src,dst,length); // executes function my_memove
-gettimeofday(&my_memmove_end, NULL);// gets time in the end
-secs  = my_memmove_end.tv_sec  - my_memmove_start.tv_sec;
-usecs = my_memmove_end.tv_usec - my_memmove_start.tv_usec;
+int32_t length=10;
+gettimeofday(&my_memzero_start, NULL); // gets time in the start
+my_memzero(src,length); // executes function my_memove
+gettimeofday(&my_memzero_end, NULL);// gets time in the end
+secs  = my_memzero_end.tv_sec  - my_memmove_start.tv_sec;
+usecs = my_memzero_end.tv_usec - my_memmove_start.tv_usec;
 time = ((secs*1000000)+usecs); // calculates time in us
 printf("Elapsed time for my_memove for %d bytes: %ld usec\n", length,time);
 }

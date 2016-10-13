108c2aab0421a57a2a9f0fb6288f7413e9ecafc1
sahanasadagopan <sahana.sadagopan@gmail.com>
2016-10-12 19:11:26 -0600

    Update profiler.c

--- profiler.c
+++ profiler.c
@@ -3,41 +3,68 @@
 #include <string.h>
 #include "memory.h"
 #include "sys/time.h"
-
-int8_t my_reverse(uint8_t *src,uint32_t length)
+int8_t* my_itoa(int8_t *str,int32_t data,int32_t base)
 {
-	uint8_t *last=src+length;;
-	uint8_t temp;
-
-	if(src!=NULL)
-	{	
-		while(src<last)
-		{
-			temp=*src;
-			*src=*last;
-			*last=temp;
-			src++;
-			last--;
-		}
-	return 1;
-	}
-	else 
-	{
-		*src=0;
-		return 0;
-	}
+   int32_t rem;
+   int num=data;
+   int8_t i=0;
+   int8_t beg,end;
+   char temp;
+   if(data<0)
+   {
+      data=-data; //if data is negative, make it positive and execute the code
+   }
+   else 
+   {
+      data=data;
+   }			
+   while(data!=0)
+   {
+      rem=data%base;
+      if(rem>9)
+      {	
+	 *(str+i)=(rem-10)+'a';
+      }
+      else
+      {
+	 *(str+i)=rem+'0';
+      }
+      data=data/base;
+      i++;
+    }
+	
+    if(base==10 && num<0) //if data is negative append a '-' to the converted data
+    {
+       *(str+i)='-';
+	i++;
+    }
+	
+    //to reverse the string
+    for(beg=0,end=i-1;beg<end;beg++,end--)
+    {
+       temp=*(str+beg);
+       *(str+beg)=*(str+end);
+       *(str+end)=temp;
+    }
+    *(str+i)='\0';
+    i++;
+    return str;
+}
+//Program to convert data from ASCII to Integer
+/*      variable definitions
+        str:pointer to output converted data*/
 }
 void main(){
 int t;
 uint8_t src[5000], dst[5000];
-struct timeval my_memmove_start,my_memmove_end,memmove_start,memmove_end,my_memzero_start,my_memzero_end,memset_start,memset_end, my_reverse_start, my_reverse_end;
+struct timeval my_atoi_start,my_atoi_end,itoa_start,itoa_end,ftoa_start,ftoa_end,memset_start,memset_end, my_reverse_start, my_reverse_end;
 long time,mtime, secs, usecs;
-int32_t length=5000;
-gettimeofday(&my_reverse_start, NULL); // gets time in the start
-my_reverse(src,length); // executes function my_memove
-gettimeofday(&my_reverse_end, NULL);// gets time in the end
-secs  = my_reverse_end.tv_sec  - my_memmove_start.tv_sec;
-usecs = my_reverse_end.tv_usec - my_memmove_start.tv_usec;
+int32_t length=10;
+gettimeofday(&my_atoi_start, NULL); // gets time in the start
+my_atoi(src,length); // executes function my_memove
+gettimeofday(&my_atoi_end, NULL);// gets time in the end
+secs  = my_atoi_end.tv_sec  - my_memmove_start.tv_sec;
+usecs = my_atoi_end.tv_usec - my_memmove_start.tv_usec;
 time = ((secs*1000000)+usecs); // calculates time in us
-printf("Elapsed time for my_reverse for %d bytes: %ld usec\n", length,time);
+printf("Elapsed time for my_atoi for %d bytes: %ld usec\n", length,time);
 }

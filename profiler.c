002f0dcc30939f171a78a80b1ffe7a29815db011
sahanasadagopan <sahana.sadagopan@gmail.com>
2016-10-12 19:56:30 -0600

    Update profiler.c

--- profiler.c
+++ profiler.c
@@ -4,38 +4,86 @@
 #include <stdlib.h>
 #include "memory.h"
 #include "sys/time.h"
-int32_t my_atoi(int8_t *str)
+void reverse(char *str, int len)
 {
-	if(str!=NULL)
-	{
-		while(*str!='\0')
-		{
-		if(*str>='0' && *str<='9' || *str=='-')
-		{
-			printf("%d",*str);
-		}
-		else return 0;
-		str++;	
-	}
-	}
-	*str=0;
+    int i=0, j=len-1, temp;
+    while (i<j)
+    {
+        temp = *(str+i);
+        *(str+i) = *(str+j);
+        *(str+j) = temp;
+        i++; j--;
+    }
 }
 
-//Program to convert data from ASCII to Integer
-/*      variable definitions
-        str:pointer to output converted data*/
+// Converts integer to ascii
+int my_itoa(char *str, int data, int d)
+{
+    int i = 0,count=0;
+    if(data<0)
+    {
+        count++;
+        data=-data;
+    }
+    while (data)
+    {
+        *(str+i++) = (data%10) + '0';
+        data = data/10;
+    }
+
+    // If number of digits required is more, then
+    // add 0s at the beginning
+    while (i < d)
+        *(str+i++) = '0';
+    if(count==1)
+        *(str+i++)='-';
+    reverse(str, i);
+    *(str+i) = '\0';
+    return i;
+}
+
+// Converts a floating point number to string.
+void my_ftoa(float data, char *str, int number_after_decimal)
+{
+    // Extract integer part
+    int before_decimal = (int)data;
+    
+    float after_decimal;
+    // Extract floating part
+    if (data<0)
+	 after_decimal= (float)before_decimal-data;
+    
+    else
+    	after_decimal = data-(float)before_decimal;
+
+    // convert integer part to string
+    int i = my_itoa(str,before_decimal, 0);
+
+    // check for numbers after point
+    if (number_after_decimal != 0)
+    {
+        *(str+i) = '.';  // add dot
+
+        // Get the value of fraction part
+        after_decimal = after_decimal * pow(10, number_after_decimal);
+
+        my_itoa(str+i+1,(int)after_decimal, number_after_decimal);
+    }
+}
 void main(){
 int t;
+	float s;
+	int dr,cr;
 int8_t src;
 int32_t dst;
 struct timeval my_atoi_start,my_atoi_end,my_itoa_start,my_itoa_end,my_ftoa_start,my_ftoa_end,atoi_start,atoi_end, itoa_start,itoa_end,ftoa_start,ftoa_end;
 long time,mtime, secs, usecs;
 int32_t base=10;
-gettimeofday(&atoi_start, NULL); // gets time in the start
-int32_t z=my_atoi(&src); // executes function my_memove
-gettimeofday(&atoi_end, NULL);// gets time in the end
-secs  = atoi_end.tv_sec  - atoi_start.tv_sec;
-usecs = atoi_end.tv_usec - atoi_start.tv_usec;
+gettimeofday(&ftoa_start, NULL); // gets time in the start
+int32_t z=my_ftoa(s,dr,cr); // executes function my_memove
+gettimeofday(&ftoa_end, NULL);// gets time in the end
+secs  = ftoa_end.tv_sec  - ftoa_start.tv_sec;
+usecs = ftoa_end.tv_usec - ftoa_start.tv_usec;
 time = ((secs*1000000)+usecs); // calculates time in us
 printf("Elapsed time for my_atoi for %d bytes: %ld usec\n", base,time);
 }
